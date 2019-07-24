13c240bb8b6b8c70b7f2a6db3f7c06f260192152
Greg Fletcher <greg.fletcher@arioplatform.com>
2019-07-24 10:40:45 -0400

    rubocop fixes

--- spec/token_validator/validator_config_spec.rb
+++ spec/token_validator/validator_config_spec.rb
@@ -12,47 +12,47 @@ RSpec.describe TokenValidator::ValidatorConfig, type: :request do
   end
 
   before do
-    TokenValidator::ValidatorConfig.configure(default_config)
+    described_class.configure(default_config)
   end
 
   it "logger set directly is not nil" do
-    TokenValidator::ValidatorConfig.logger = Logger.new(STDOUT)
-    expect(TokenValidator::ValidatorConfig.logger).not_to be nil
-    expect(TokenValidator::ValidatorConfig.logger.is_a?(Logger)).to be true
+    described_class.logger = Logger.new(STDOUT)
+    expect(described_class.logger).not_to be nil
+    expect(described_class.logger.is_a?(Logger)).to be true
   end
 
   it 'config does not set key/value for unknown key' do
-    TokenValidator::ValidatorConfig.configure(foo: 'bar')
-    expect(TokenValidator::ValidatorConfig.config).to eq(default_config)
+    described_class.configure(foo: 'bar')
+    expect(described_class.config).to eq(default_config)
   end
 
   it 'config sets issuer_url to a known value' do
-    TokenValidator::ValidatorConfig.configure(issuer_url: 'https://example.com')
-    expect(TokenValidator::ValidatorConfig.config).to have_key(:issuer_url)
-    expect(TokenValidator::ValidatorConfig.config).to have_value('https://example.com')
+    described_class.configure(issuer_url: 'https://example.com')
+    expect(described_class.config).to have_key(:issuer_url)
+    expect(described_class.config).to have_value('https://example.com')
   end
 
   it 'config sets client ID to a known value' do
-    TokenValidator::ValidatorConfig.configure(client_id: 'abc123')
-    expect(TokenValidator::ValidatorConfig.config).to have_key(:client_id)
-    expect(TokenValidator::ValidatorConfig.config).to have_value('abc123')
+    described_class.configure(client_id: 'abc123')
+    expect(described_class.config).to have_key(:client_id)
+    expect(described_class.config).to have_value('abc123')
   end
 
   it 'config sets client secret to a known value' do
-    TokenValidator::ValidatorConfig.configure(client_secret: 'secret123')
-    expect(TokenValidator::ValidatorConfig.config).to have_key(:client_secret)
-    expect(TokenValidator::ValidatorConfig.config).to have_value('secret123')
+    described_class.configure(client_secret: 'secret123')
+    expect(described_class.config).to have_key(:client_secret)
+    expect(described_class.config).to have_value('secret123')
   end
 
   it 'config sets requested scope to a known value' do
-    TokenValidator::ValidatorConfig.configure(requested_scope: 'test:scope')
-    expect(TokenValidator::ValidatorConfig.config).to have_key(:requested_scope)
-    expect(TokenValidator::ValidatorConfig.config).to have_value('test:scope')
+    described_class.configure(requested_scope: 'test:scope')
+    expect(described_class.config).to have_key(:requested_scope)
+    expect(described_class.config).to have_value('test:scope')
   end
 
   it 'config sets audience to a known value' do
-    TokenValidator::ValidatorConfig.configure(audience: 'https://localhost:3000')
-    expect(TokenValidator::ValidatorConfig.config).to have_key(:audience)
-    expect(TokenValidator::ValidatorConfig.config).to have_value('https://localhost:3000')
+    described_class.configure(audience: 'https://localhost:3000')
+    expect(described_class.config).to have_key(:audience)
+    expect(described_class.config).to have_value('https://localhost:3000')
   end
 end
